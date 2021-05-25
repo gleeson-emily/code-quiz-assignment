77314f7a0a1949d1696e004767e8d06cd43738e1
Emily Gleeson <engleeson@gmail.com>
2021-05-25 09:35:34 -0400

    setting up documents and skeleton html

---
+++ logic.js
@@ -0,0 +1,10 @@
+//functions: timer function, start all function, game function
+
+var startButton = document.getElementById(questionStart);
+var questionSection = document.getElementById(questions);
+var timerCountdown = document.getElementById(timerDiv);
+
+
+document.addEventListener("questionSection", "")
+//insert function to start timer when this is pushed
+
